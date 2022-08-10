ba2ec2aea8f3a33b04f1115d0643022c30f3e49c
aparna0202 <aparna.pandey172004@gmail.com>
2022-08-10 16:21:00 +0530

    .

--- src/components/WishlistPage/WishlistPage.jsx
+++ src/components/WishlistPage/WishlistPage.jsx
@@ -5,49 +5,33 @@ import { useEffect, useState } from "react";
 const Test = () => {
   return <h1>Hello</h1>;
 };
-const WishlistPage = () => {
-  const wishlistedIsbn = JSON.parse(localStorage.getItem("wishlist"));
-  const [wishlistedBook, setWishlistedBook] = useState([]);
 
-  useEffect(() => {
+const WishlistPage = () => {
+  const [wishlistedBook, setWishlistedBook] = useState(
+    () => getBooksfromISBN() || []
+  );
+
+  function getBooksfromISBN() {
     const fetchedResult = [];
+    const wishlistedIsbn = JSON.parse(localStorage.getItem("wishlist"));
+
     wishlistedIsbn.map((isbn) =>
       fetch(`${API_URL}?q=isbn:${isbn}`).then((result) =>
         result.json().then((result) => fetchedResult.push(result))
       )
     );
+    return fetchedResult;
+  }
 
-    // console.log(fetchedResult);
-    setWishlistedBook(fetchedResult);
-  }, []);
+  useEffect(() => {
+    getBooksfromISBN();
+  });
+  console.log(wishlistedBook.length);
 
   return (
     <div className="WishlistPage">
-      {wishlistedBook.length > 0 &&
-        wishlistedBook.map(
-          (book) => <Test />
-          // <Book
-          //   authorName={
-          //     book.items[0].volumeInfo.authors
-          //       ? book.items[0].volumeInfo.authors[0]
-          //       : "N/A"
-          //   }
-          //   publishedDate={
-          //     book.items[0].volumeInfo.publishedDate
-          //       ? book.items[0].volumeInfo.publishedDate
-          //       : "NA"
-          //   }
-          //   bookName={book.items[0].volumeInfo.title}
-          //   publisher={book.items[0].volumeInfo.publisher}
-          //   description={book.items[0].volumeInfo.description}
-          //   subtitle={book.items[0].volumeInfo.subtitle}
-          //   imageLinks={book.items[0].volumeInfo.imageLinks}
-          //   pageCount={book.items[0].volumeInfo.pageCount}
-          //   industryIdentifiers={
-          //     book.items[0].volumeInfo.industryIdentifiers[0].identifier
-          //   }
-          // />
-        )}
+      {console.log(wishlistedBook.length, wishlistedBook)}
+      {wishlistedBook.map((data) => console.log(data))}
     </div>
   );
 };
