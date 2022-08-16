0e469e6566727a990f01d15623b08839bf21f058
aparna0202 <aparna.pandey172004@gmail.com>
2022-08-16 10:47:42 +0530

    .

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -5,16 +5,9 @@ import "./Cover.css";
 const Cover = () => {
   return (
     <div className="cover">
-      <div className="search">
-        <Searchbar />
-      </div>
-      <div className="imgContainer">
-        <img
-          className="img"
-          src="https://images.unsplash.com/photo-1533669955142-6a73332af4db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80"
-          alt="Cover"
-        />
-      </div>
+      <div className="image"></div>
+      <Searchbar />
+
       <div className="textContainer">
         <blockquote>
           So many books, so little time.
